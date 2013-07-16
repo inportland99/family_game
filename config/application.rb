ec32826e3cea6ff745aa1a29a965e38f30323fe8
Raj Shah <drrajshah@me.com>
2013-07-15 22:26:14 -0400

    precompile false in application.rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -55,8 +55,10 @@ module FamilyGame
 
     # Enable the asset pipeline
     config.assets.enabled = true
-
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
+
   end
 end
